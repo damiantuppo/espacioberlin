70ce45ea694db95781c779601467b84880ec4958
Damian Tuppo <berlininstrumentos@gmail.com>
2020-08-31 16:25:02 -0300

    entrega final!

---
+++ envia_n.php
@@ -0,0 +1,26 @@
+﻿<?php
+$remitente = $_POST['email'];
+$destinatario = 'berlininstrumentos@gmail.com'; 
+$asunto = 'Consulta';
+if (!$_POST){
+?>
+
+<?php
+}else{
+	 
+    $cuerpo = "Nombre y apellido: " . $_POST["nombre"] . "\r\n"; 
+    $cuerpo .= "Email: " . $_POST["email"] . "\r\n";
+    $cuerpo .= "Telefono " . $_POST["telefono"] . "\r\n";
+	$cuerpo .= "Consulta: " . $_POST["consulta"] . "\r\n";
+	$headers  = "MIME-Version: 1.0\n";
+    $headers .= "Content-type: text/plain; charset=utf-8\n";
+    $headers .= "X-Priority: 3\n";
+    $headers .= "X-MSMail-Priority: Normal\n";
+    $headers .= "X-Mailer: php\n";
+    $headers .= "From: \"".$_POST['nombre']." ".$_POST['apellido']."\" <".$remitente.">\n";
+
+    mail($destinatario, $asunto, $cuerpo, $headers);
+    
+    include 'confirma_n.html';
+}
+?>
